3ab4a13ca8d07fe4cd548229fb477860028fdf22
Johan Swan <jswannieman@gmail.com>
2024-01-18 11:16:00 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,11 @@ function App() {
     <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-10">
             <ThemeBtn />
           </div>
 
-          <div className="w-full max-w-sm mx-auto">
+          <div className="w-full max-w-2xl mx-auto">
             <Card />
           </div>
         </div>
