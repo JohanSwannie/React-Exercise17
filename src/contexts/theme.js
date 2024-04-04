8f3e49914af3c038ef3ca36cbc69004aa67827bf
Johan Swan <jswannieman@gmail.com>
2024-04-05 11:42:16 +1300

    Update theme JS Component

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -1,6 +1,6 @@
 import { createContext, useContext } from "react";
 
-export const ThemeContext = createContext({
+const ThemeContext = createContext({
   themeMode: "light",
   darkTheme: () => {},
   lightTheme: () => {},
